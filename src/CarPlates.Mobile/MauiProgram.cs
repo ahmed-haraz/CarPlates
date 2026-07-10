119af875e5fc38e9e941b4e4aed053647b620d14
Reham <Reham@HP>
2026-07-10 11:07:38 +0300

    0

--- src/CarPlates.Mobile/MauiProgram.cs
+++ src/CarPlates.Mobile/MauiProgram.cs
@@ -48,7 +48,7 @@ public static class MauiProgram
         // Database path
         var dbPath = Path.Combine(FileSystem.AppDataDirectory, StorageConstants.DatabaseName);
 
-        Preferences.Remove("api_url");
+        //Preferences.Remove("api_url");
 
         var apiUrl = Preferences.Get("api_url", ApiConstants.DefaultApiUrl);
 
