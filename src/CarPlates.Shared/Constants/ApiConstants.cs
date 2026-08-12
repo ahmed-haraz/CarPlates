ec8a5fee628d7275a1596a5e29d8ef31674bb553
Reham <Reham@HP>
2026-08-12 08:47:19 +0300

    0

--- src/CarPlates.Shared/Constants/ApiConstants.cs
+++ src/CarPlates.Shared/Constants/ApiConstants.cs
@@ -3,7 +3,7 @@ namespace CarPlates.Shared.Constants;
 public static class ApiConstants
 {
     public const string ApiVersion = "v1";
-    public const string DefaultApiUrl = $"https://online.arkancloud.com:7072/api/{ApiVersion}/";
+    public const string DefaultApiUrl = $"https://hexa2025.dyndns.org:8052/api/{ApiVersion}/";
     public const int TimeoutSeconds = 30;
     public const int MaxRetryCount = 3;
     public const int RetryDelayMs = 1000;
